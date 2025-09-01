7708ae192b8aac533816909e0ef3608d64b925fc
charlysdark22 <carlosaguilardark22@gmail.com>
2025-09-01 10:10:26 -0400

    1

--- store/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    'postcss-preset-env': { stage: 3, features: { 'nesting-rules': true } },
-  }
-}
